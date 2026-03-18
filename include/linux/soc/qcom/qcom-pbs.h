0e8361a395b825e6ad16a62ab48586446f4d27be
ZXlieC <xlie7669@gmail.com>
2026-03-18 22:20:56 +0200

    input: misc: add qcom-hv-haptics support

--- include/linux/soc/qcom/qcom-pbs.h
+++ include/linux/soc/qcom/qcom-pbs.h
@@ -14,9 +14,15 @@ struct pbs_dev;
 
 #if IS_ENABLED(CONFIG_QCOM_PBS)
 int qcom_pbs_trigger_event(struct pbs_dev *pbs, u8 bitmap);
+int qcom_pbs_trigger_single_event(struct pbs_dev *pbs);
 struct pbs_dev *get_pbs_client_device(struct device *client_dev);
 #else
 static inline int qcom_pbs_trigger_event(struct pbs_dev *pbs, u8 bitmap)
+{
+	return -ENODEV;
+}
+
+static inline int qcom_pbs_trigger_single_event(struct pbs_dev *pbs)
 {
 	return -ENODEV;
 }
